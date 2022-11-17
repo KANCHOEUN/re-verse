245ca8aee20808373011192f390d970d8477ef68
지용현 <yung5487@gmail.com>
2022-11-17 17:48:22 +0900

    [FE] feat: create loading page
    Former-commit-id: f209d89f9c1a30ef0f318ea0b0a6a62e913d4387

---
+++ frontend/src/UI/organisms/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="bg-red-300 flex absolute w-full h-full justify-center align-middle z-50 items-center overflow-x-hidden overflow-y-hidden">
+      로딩페이지입니다.
+    </div>
+  );
+}
+
+export default Loading;
